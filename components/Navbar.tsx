ec78adce8cba813ac8d9f1c283e2eea81d4a99c6
mihue14 <samirjose675@gmail.com>
2022-12-16 17:17:09 -0300

    Componente Navbar arreglado

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../assets/logo.png";
 import MenuDropdown from "./MenuDropdown";
+import background from "../assets/background.jpg";
 
 const Navbar = () => {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    window.onscroll = function () {
+      setScrollY(window.scrollY);
+    };
+  }, [scrollY]);
+
   return (
-    <nav className="flex justify-around w-full sticky top-0 bg-black bg-opacity-70">
+    <nav
+      className={`flex justify-around w-full h-full sticky z-50 top-0 bg-black ${
+        scrollY < 200 ? "bg-opacity-100" : "bg-opacity-80"
+      }`}
+    >
       {/* MOBILE */}
       <MenuDropdown />
       <img src={logo.src} alt="logo" className="h-auto w-auto lg:hidden" />
@@ -16,28 +29,16 @@ const Navbar = () => {
       </a>
 
       {/* PC */}
-      <a
-        href="/"
-        className="my-auto text-white max-md:hidden hover:rounded-lg hover:bg-black p-1 transition-all duration-500"
-      >
+      <a className="my-auto text-white max-md:hidden max-lg:hidden hover:rounded-lg hover:bg-gray-800 p-1 transition-all duration-500">
         INICIO
       </a>
-      <a
-        href="/nosotros"
-        className="my-auto text-white max-md:hidden hover:rounded-lg hover:bg-black p-1 transition-all duration-500"
-      >
+      <a className="my-auto text-white max-md:hidden max-lg:hidden hover:rounded-lg hover:bg-gray-800 p-1 transition-all duration-500">
         NOSOTROS
       </a>
-      <a
-        href="/horarios"
-        className="my-auto text-white max-md:hidden hover:rounded-lg hover:bg-black p-1 transition-all duration-500"
-      >
+      <a className="my-auto text-white max-md:hidden max-lg:hidden hover:rounded-lg hover:bg-gray-800 p-1 transition-all duration-500">
         HORARIOS
       </a>
-      <a
-        href="/equipo"
-        className="my-auto text-white max-md:hidden hover:rounded-lg hover:bg-black p-1 transition-all duration-500"
-      >
+      <a className="my-auto text-white max-md:hidden max-lg:hidden hover:rounded-lg hover:bg-gray-800 p-1 transition-all duration-500">
         EQUIPO
       </a>
       <img
@@ -45,27 +46,18 @@ const Navbar = () => {
         alt="logo"
         className="h-auto w-auto hidden lg:block"
       />
-      <a
-        href="/servicios"
-        className="my-auto text-white max-md:hidden hover:rounded-lg hover:bg-black p-1 transition-all duration-500"
-      >
+      <a className="my-auto text-white max-md:hidden max-lg:hidden hover:rounded-lg hover:bg-gray-800 p-1 transition-all duration-500">
         SERVICIOS
       </a>
-      <a
-        href="/galeria"
-        className="my-auto text-white max-md:hidden hover:rounded-lg hover:bg-black p-1 transition-all duration-500"
-      >
+      <a className="my-auto text-white max-md:hidden max-lg:hidden hover:rounded-lg hover:bg-gray-800 p-1 transition-all duration-500">
         GALERIA
       </a>
-      <a
-        href="/contacto"
-        className="my-auto text-white max-md:hidden hover:rounded-lg hover:bg-black p-1 transition-all duration-500"
-      >
+      <a className="my-auto text-white max-md:hidden max-lg:hidden hover:rounded-lg hover:bg-gray-800 p-1 transition-all duration-500">
         CONTACO
       </a>
       <a
         href="/reservar"
-        className="my-auto p-1 rounded-md bg-white text-black max-md:hidden hover:bg-black hover:text-white transition-all duration-500"
+        className="my-auto p-1 rounded-md bg-white text-black max-md:hidden max-lg:hidden hover:bg-gray-800 hover:text-white transition-all duration-500"
       >
         ¡RESERVAR YA!
       </a>
