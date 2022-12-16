fd12aeecb9a5840adf4137a8898a21a2eea85cf5
mihue14 <samirjose675@gmail.com>
2022-12-16 17:19:27 -0300

    Componente Schedule eliminado

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import Head from "next/head";
 import NavbarComponent from "../components/Navbar";
 import HomeComponent from "../components/Home";
 import AboutUsComponent from "../components/AboutUs";
-import ScheduleComponent from "../components/Schedule";
 
 export default function Home() {
   return (
