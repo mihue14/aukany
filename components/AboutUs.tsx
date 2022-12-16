216a35cb680780d981c3b2bc4369636422d11b7d
mihue14 <samirjose675@gmail.com>
2022-12-16 17:14:58 -0300

    Componente AboutUs

---
+++ components/AboutUs.tsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { Babang } from "../fonts";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1 className={`${Babang.className} text-2xl mb-5`}>SOBRE NOSOTROS 💈</h1>
+      <span className="text-sm font-serif">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id magna
+        non urna tristique ullamcorper. Nam consequat lobortis lectus ac
+        facilisis. Fusce leo est, aliquam sodales pretium vitae, tincidunt non
+        erat. Nam congue justo eget consequat viverra. Quisque sodales facilisis
+        pellentesque. Nulla facilisi. Vestibulum sit amet ultrices lacus.
+        Pellentesque dictum lorem non rhoncus dapibus. Nullam placerat tortor
+        felis, eget porta odio imperdiet vel. Aenean in sapien sollicitudin
+        purus porttitor sollicitudin eu a tellus. Aliquam vitae sem et nisl
+        dictum lacinia tincidunt vitae metus. Etiam a velit pellentesque,
+        commodo risus vel, dapibus lorem. Proin et porttitor eros. In hac
+        habitasse platea dictumst. Donec luctus elit quis neque auctor
+        dignissim. Sed sed lectus eu quam pellentesque egestas sed sit amet
+        mauris. Morbi nec dignissim purus. Vestibulum ultrices convallis dui at
+        tristique. Aliquam erat volutpat. Donec condimentum, erat interdum
+        tincidunt egestas, dui mauris rutrum odio, vehicula sodales elit nisl ac
+        ex. Nulla lacinia augue libero, eget varius diam consectetur sed. Aenean
+        tincidunt est vel nunc congue mollis. Interdum et malesuada fames ac
+        ante ipsum primis in faucibus. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Sed id magna non urna tristique ullamcorper. Nam
+        consequat lobortis lectus ac facilisis. Fusce leo est, aliquam sodales
+        pretium vitae, tincidunt non erat. Nam congue justo eget consequat
+        viverra. Quisque sodales facilisis pellentesque. Nulla facilisi.
+        Vestibulum sit amet ultrices lacus. Pellentesque dictum lorem non
+        rhoncus dapibus. Nullam placerat tortor felis, eget porta odio imperdiet
+        vel. Aenean in sapien sollicitudin purus porttitor sollicitudin eu a
+        tellus. Aliquam vitae sem et nisl dictum lacinia tincidunt vitae metus.
+        Etiam a velit pellentesque, commodo risus vel, dapibus lorem. Proin et
+        porttitor eros. In hac habitasse platea dictumst. Donec luctus elit quis
+        neque auctor dignissim. Sed sed lectus eu quam pellentesque egestas sed
+        sit amet mauris. Morbi nec dignissim purus. Vestibulum ultrices
+        convallis dui at tristique. Aliquam erat volutpat. Donec condimentum,
+        erat interdum tincidunt egestas, dui mauris rutrum odio, vehicula
+        sodales elit nisl ac ex. Nulla lacinia augue libero, eget varius diam
+        consectetur sed. Aenean tincidunt est vel nunc congue mollis. Interdum
+        et malesuada fames ac ante ipsum primis in faucibus. Lorem ipsum dolor
+        sit amet, consectetur adipiscing elit. Sed id magna non urna tristique
+        ullamcorper. Nam consequat lobortis lectus ac facilisis. Fusce leo est,
+        aliquam sodales pretium vitae, tincidunt non erat. Nam congue justo eget
+        consequat viverra. Quisque sodales facilisis pellentesque. Nulla
+        facilisi. Vestibulum sit amet ultrices lacus. Pellentesque dictum lorem
+        non rhoncus dapibus. Nullam placerat tortor felis, eget porta odio
+        imperdiet vel. Aenean in sapien sollicitudin purus porttitor
+        sollicitudin eu a tellus. Aliquam vitae sem et nisl dictum lacinia
+        tincidunt vitae metus. Etiam a velit pellentesque, commodo risus vel,
+        dapibus lorem. Proin et porttitor eros. In hac habitasse platea
+        dictumst. Donec luctus elit quis neque auctor dignissim. Sed sed lectus
+        eu quam pellentesque egestas sed sit amet mauris. Morbi nec dignissim
+        purus. Vestibulum ultrices convallis dui at tristique. Aliquam erat
+        volutpat. Donec condimentum, erat interdum tincidunt egestas, dui mauris
+        rutrum odio, vehicula sodales elit nisl ac ex. Nulla lacinia augue
+        libero, eget varius diam consectetur sed. Aenean tincidunt est vel nunc
+        congue mollis. Interdum et malesuada fames ac ante ipsum primis in
+        faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+        id magna non urna tristique ullamcorper. Nam consequat lobortis lectus
+        ac facilisis. Fusce leo est, aliquam sodales pretium vitae, tincidunt
+        non erat. Nam congue justo eget consequat viverra. Quisque sodales
+        facilisis pellentesque. Nulla facilisi. Vestibulum sit amet ultrices
+        lacus. Pellentesque dictum lorem non rhoncus dapibus. Nullam placerat
+        tortor felis, eget porta odio imperdiet vel. Aenean in sapien
+        sollicitudin purus porttitor sollicitudin eu a tellus. Aliquam vitae sem
+        et nisl dictum lacinia tincidunt vitae metus. Etiam a velit
+        pellentesque, commodo risus vel, dapibus lorem. Proin et porttitor eros.
+        In hac habitasse platea dictumst. Donec luctus elit quis neque auctor
+        dignissim. Sed sed lectus eu quam pellentesque egestas sed sit amet
+        mauris. Morbi nec dignissim purus. Vestibulum ultrices convallis dui at
+        tristique. Aliquam erat volutpat. Donec condimentum, erat interdum
+        tincidunt egestas, dui mauris rutrum odio, vehicula sodales elit nisl ac
+        ex. Nulla lacinia augue libero, eget varius diam consectetur sed. Aenean
+        tincidunt est vel nunc congue mollis. Interdum et malesuada fames ac
+        ante ipsum primis in faucibus. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Sed id magna non urna tristique ullamcorper. Nam
+        consequat lobortis lectus ac facilisis. Fusce leo est, aliquam sodales
+        pretium vitae, tincidunt non erat. Nam congue justo eget consequat
+        viverra. Quisque sodales facilisis pellentesque. Nulla facilisi.
+        Vestibulum sit amet ultrices lacus. Pellentesque dictum lorem non
+        rhoncus dapibus. Nullam placerat tortor felis, eget porta odio imperdiet
+        vel. Aenean in sapien sollicitudin purus porttitor sollicitudin eu a
+        tellus. Aliquam vitae sem et nisl dictum lacinia tincidunt vitae metus.
+        Etiam a velit pellentesque, commodo risus vel, dapibus lorem. Proin et
+        porttitor eros. In hac habitasse platea dictumst. Donec luctus elit quis
+        neque auctor dignissim. Sed sed lectus eu quam pellentesque egestas sed
+        sit amet mauris. Morbi nec dignissim purus. Vestibulum ultrices
+        convallis dui at tristique. Aliquam erat volutpat. Donec condimentum,
+        erat interdum tincidunt egestas, dui mauris rutrum odio, vehicula
+        sodales elit nisl ac ex. Nulla lacinia augue libero, eget varius diam
+        consectetur sed. Aenean tincidunt est vel nunc congue mollis. Interdum
+        et malesuada fames ac ante ipsum primis in faucibus.
+      </span>
+    </div>
+  );
+};
+
+export default AboutUs;
