027d0f3666953f9d2f45cd63b0acd7105f07b07b
mihue14 <samirjose675@gmail.com>
2022-12-19 19:32:39 -0300

    Servicios

---
+++ components/Services.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import services from "../assets/services.png";
+
+const Services = () => {
+  return (
+    <div>
+      <img src={services.src} alt="services" />
+    </div>
+  );
+};
+
+export default Services;
