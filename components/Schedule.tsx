b49cd25395354a9c462aa7384b96d786c5a3defd
mihue14 <samirjose675@gmail.com>
2022-12-19 19:31:53 -0300

    Horarios

---
+++ components/Schedule.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import schedule from "../assets/schedule.png";
+import { Flathead } from "../fonts";
+
+const Schedule = () => {
+  return (
+    <div className="bg-black p-10 rounded-md mt-10 text-center">
+      <img
+        src={schedule.src}
+        alt="schedule"
+        className="mb-5 h-20 block mx-auto"
+      />
+      <span className={`${Flathead.className} text-white mx-auto text-3xl`}>
+        TODOS LOS DÍAS DE
+        <br />
+        09:00hs a 13:00hs
+        <br />
+        7:00hs a 21:00hs
+      </span>
+    </div>
+  );
+};
+
+export default Schedule;
