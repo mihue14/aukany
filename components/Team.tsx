69fead3b6cbcdd58509d8501dfa7d58278370638
mihue14 <samirjose675@gmail.com>
2022-12-19 19:32:02 -0300

    Equipo

---
+++ components/Team.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import team from "../assets/team.png";
+import DemoCarousel from "./DemoCarousel";
+
+const Team = () => {
+  return (
+    <div className="p-10 text-center">
+      <img
+        src={team.src}
+        alt="team"
+        className="h-28 block mx-auto w-full md:h-32 md:w-1/2 lg:w-1/5 lg:mt-10 lg:h-24"
+      />
+      <DemoCarousel />
+    </div>
+  );
+};
+
+export default Team;
