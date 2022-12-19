d61b2db4a0ba53cd667885a32eb80ed537e3494c
mihue14 <samirjose675@gmail.com>
2022-12-19 19:32:24 -0300

    Componentes agregados a index

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,19 @@ import Head from "next/head";
 import NavbarComponent from "../components/Navbar";
 import HomeComponent from "../components/Home";
 import AboutUsComponent from "../components/AboutUs";
+import ScheduleComponent from "../components/Schedule";
+import TeamComponent from "../components/Team";
+import ServicesComponent from "../components/Services";
 
 export default function Home() {
   return (
-    <div>
+    <div className="border border-black">
       <NavbarComponent />
       <HomeComponent />
       <AboutUsComponent />
+      <ScheduleComponent />
+      <TeamComponent />
+      <ServicesComponent />
     </div>
   );
 }
