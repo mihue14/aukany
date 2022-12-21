9adde2634cbe68ce6321ca24c8ca143606757c80
mihue14 <samirjose675@gmail.com>
2022-12-21 12:48:32 -0300

    Ícono agregado

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,19 +8,27 @@ import ServicesComponent from "../components/Services";
 import GalleryComponent from "../components/Gallery";
 import ContactComponent from "../components/Contact";
 import FooterComponent from "../components/Footer";
+import logo from "../assets/logo2.png";
 
 export default function Home() {
   return (
-    <div className="border border-black">
-      <NavbarComponent />
-      <HomeComponent />
-      <AboutUsComponent id="1" />
-      <ScheduleComponent id="2" />
-      <TeamComponent id="3" />
-      <ServicesComponent id="4" />
-      <GalleryComponent id="5" />
-      <ContactComponent id="6" />
-      <FooterComponent />
-    </div>
+    <>
+      <Head>
+        <title>Aukany</title>
+        <meta name="google" content="notranslate" />
+        <link rel="shortcut icon" href={logo.src} />
+      </Head>
+      <div className="border border-black">
+        <NavbarComponent />
+        <HomeComponent />
+        <AboutUsComponent id="1" />
+        <ScheduleComponent id="2" />
+        <TeamComponent id="3" />
+        <ServicesComponent id="4" />
+        <GalleryComponent id="5" />
+        <ContactComponent id="6" />
+        <FooterComponent />
+      </div>
+    </>
   );
 }
