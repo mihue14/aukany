72a40a5bda92881f32d5a0440d23a33b816ab6cc
mihue14 <samirjose675@gmail.com>
2022-12-23 04:06:42 -0300

    Interfaces

---
+++ interfaces/bookings.ts
@@ -0,0 +1,8 @@
+export interface IBookings {
+  name: string;
+  lastname: string;
+  service: string;
+  phone: string;
+  day: string | string[];
+  hour: string;
+}
---
+++ interfaces/input.ts
@@ -0,0 +1,8 @@
+export interface IInput {
+  name: string;
+  lastname: string;
+  service: string;
+  phone: string;
+  day: string;
+  hour: string;
+}
