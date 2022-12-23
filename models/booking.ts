a774c5c95696025008ad36695d88f30460a3ac1b
mihue14 <samirjose675@gmail.com>
2022-12-23 04:07:01 -0300

    Modelos

---
+++ models/booking.ts
@@ -0,0 +1,51 @@
+import moment from "moment";
+import { Schema, model, models } from "mongoose";
+
+const bookingSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    service: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    day: {
+      type: String,
+      required: true,
+    },
+    hour: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: String,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: String,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+bookingSchema.pre("save", function (next) {
+  this.createdAt = moment().format();
+  this.updatedAt = moment().format();
+  next();
+});
+
+export default models["Booking"] || model("Booking", bookingSchema);
