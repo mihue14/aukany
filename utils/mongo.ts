8ad384bfbe028382ad2a5b79a997be568ed6b2c0
mihue14 <samirjose675@gmail.com>
2022-12-23 15:45:37 -0300

    Mongo caché arreglado

--- utils/mongo.ts
+++ utils/mongo.ts
@@ -26,8 +26,10 @@ async function dbConnect() {
 
   if (!cached.promise) {
     const opts = {
-      bufferCommands: false,
+      bufferCommands: true,
     };
+
+    mongoose.set("strictQuery", false);
 
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
       return mongoose;
