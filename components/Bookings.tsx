dbcd510cb9a442d7fd905e3053e79cd862c5ff46
mihue14 <samirjose675@gmail.com>
2022-12-31 18:53:53 -0300

    Arreglo para obtener las reservas del state

--- components/Bookings.tsx
+++ components/Bookings.tsx
@@ -1,38 +1,55 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Flathead } from "../fonts";
-import { pagination } from "../hooks/pagination";
 import { BarLoader } from "react-spinners";
+import { useAppDispatch } from "../hooks";
+import { getAllBookings, getBookings } from "../redux/actions/bookingsActions";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+import { IBookings } from "../interfaces/bookings";
+import Pagination from "./Pagination";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState({
-    name: [],
-    lastname: [],
-    service: [],
-    day: [],
-    hour: [],
-  });
-  const [allBookings, setAllBookings] = useState([]);
-  const [page, setPage] = useState(0);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(getAllBookings());
+    dispatch(getBookings(0));
+  }, []);
+
+  const bookings = useSelector(
+    (state: RootState) => state.bookingReducer.bookings
+  );
+
   const [loading, setLoading] = useState(false);
+  const [bookingsPerPage, setBookingsPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
+  const indexOfLastBooking = currentPage * bookingsPerPage;
+  const indexOfFirstBooking = indexOfLastBooking - bookingsPerPage;
+  const currentBookings = bookings.slice(
+    indexOfFirstBooking,
+    indexOfLastBooking
+  );
 
   useEffect(() => {
-    axios.get("/api/booking").then(({ data }) => {
-      setBookings({
-        name: data.bookings.map((d: { name: string }) => d.name),
-        lastname: data.bookings.map((d: { lastname: string }) => d.lastname),
-        service: data.bookings.map((d: { service: string }) => d.service),
-        day: data.bookings.map((d: { day: string }) => d.day),
-        hour: data.bookings.map((d: { hour: string }) => d.hour),
-      });
-      setAllBookings(data.allBookings);
-    });
-  }, []);
+    setCurrentPage(1);
+  }, [bookings]);
+
+  const prev = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const next = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const waitPaged = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 250);
+  };
 
   return (
     <div className="text-center">
-      <span className={`${Flathead.className} text-2xl`}>Turnos:</span>
-
       {loading ? (
         <BarLoader
           cssOverride={{
@@ -45,93 +62,98 @@ const Bookings = () => {
           }}
         />
       ) : (
-        <div className="flex justify-around">
-          <div className="p-1">
-            <span className="font-bold">Nombre</span>
-            {bookings.name.length > 0 &&
-              bookings.name.map((n, index) => {
-                return <div key={index}>{n}</div>;
-              })}
+        <>
+          {/* PC */}
+          <div className="lg:flex lg:justify-around max-lg:hidden">
+            <div className="lg:p-1">
+              <span className="font-bold">Nombre</span>
+              {currentBookings.length > 0 &&
+                currentBookings.map((n: IBookings, index) => {
+                  return <div key={index}>{n.name}</div>;
+                })}
+            </div>
+
+            <div className="lg:p-1">
+              <span className="font-bold">Apellido</span>
+              {currentBookings.length > 0 &&
+                currentBookings.map((l: IBookings, index) => {
+                  return <div key={index}>{l.lastname}</div>;
+                })}
+            </div>
+
+            <div className="lg:p-1">
+              <span className="font-bold">Celular</span>
+              {currentBookings.length > 0 &&
+                currentBookings.map((p: IBookings, index) => {
+                  return <div key={index}>+{p.phone}</div>;
+                })}
+            </div>
+
+            <div className="lg:p-1">
+              <span className="font-bold">Servicio</span>
+              {currentBookings.length > 0 &&
+                currentBookings.map((s: IBookings, index) => {
+                  return <div key={index}>{s.service}</div>;
+                })}
+            </div>
+
+            <div className="lg:p-1">
+              <span className="font-bold">Días</span>
+              {currentBookings.length > 0 &&
+                currentBookings.map((d: IBookings, index) => {
+                  return <div key={index}>{d.day}</div>;
+                })}
+            </div>
+
+            <div className="lg:p-1">
+              <span className="font-bold">Hora</span>
+              {currentBookings.length > 0 &&
+                currentBookings.map((h: IBookings, index) => {
+                  return <div key={index}>{h.hour}</div>;
+                })}
+            </div>
           </div>
 
-          <div className="p-1">
-            <span className="font-bold">Apellido</span>
-            {bookings.lastname.length > 0 &&
-              bookings.lastname.map((l, index) => {
-                return <div key={index}>{l}</div>;
+          {/* CELULAR */}
+          <div className="min-[640px]:hidden">
+            {currentBookings.length > 0 &&
+              currentBookings.map((elem: IBookings, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="border rounded-lg border-black p-2 my-5"
+                  >
+                    <span className="font-bold">Nombre: </span>
+                    {elem.name}
+                    <br />
+                    <span className="font-bold">Apellido: </span>
+                    {elem.lastname}
+                    <br />
+                    <span className="font-bold">Celular: </span>+{elem.phone}
+                    <br />
+                    <span className="font-bold">Servicio: </span>
+                    {elem.service}
+                    <br />
+                    <span className="font-bold">Día: </span>
+                    {elem.day}
+                    <br />
+                    <span className="font-bold">Hora: </span>
+                    {elem.hour}
+                  </div>
+                );
               })}
           </div>
-
-          <div className="p-1">
-            <span className="font-bold">Servicio</span>
-            {bookings.service.length > 0 &&
-              bookings.service.map((s, index) => {
-                return <div key={index}>{s}</div>;
-              })}
-          </div>
-
-          <div className="p-1">
-            <span className="font-bold">Días</span>
-            {bookings.day.length > 0 &&
-              bookings.day.map((d, index) => {
-                return <div key={index}>{d}</div>;
-              })}
-          </div>
-
-          <div className="p-1">
-            <span className="font-bold">Hora</span>
-            {bookings.hour.length > 0 &&
-              bookings.hour.map((h, index) => {
-                return <div key={index}>{h}</div>;
-              })}
-          </div>
-        </div>
+        </>
       )}
 
-      <div className="flex justify-center p-5">
-        <button
-          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l mx-5"
-          onClick={async () => {
-            if (page >= 1) {
-              setLoading(true);
-              setBookings({
-                name: (await pagination(page - 1)).name,
-                lastname: (await pagination(page - 1)).lastname,
-                service: (await pagination(page - 1)).service,
-                day: (await pagination(page - 1)).day,
-                hour: (await pagination(page - 1)).hour,
-              });
-              setPage(page - 1);
-              setTimeout(() => {
-                setLoading(false);
-              }, 10);
-            }
-          }}
-        >
-          {"<"}
-        </button>
-        <button
-          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l mx-5"
-          onClick={async () => {
-            if (page < Math.floor(allBookings.length / 5)) {
-              setLoading(true);
-              setBookings({
-                name: (await pagination(page + 1)).name,
-                lastname: (await pagination(page + 1)).lastname,
-                service: (await pagination(page + 1)).service,
-                day: (await pagination(page + 1)).day,
-                hour: (await pagination(page + 1)).hour,
-              });
-              setPage(page + 1);
-              setTimeout(() => {
-                setLoading(false);
-              }, 10);
-            }
-          }}
-        >
-          {">"}
-        </button>
-      </div>
+      <Pagination
+        allBookings={bookings.length}
+        bookingsPerPage={bookingsPerPage}
+        currentPage={currentPage}
+        prev={prev}
+        next={next}
+        waitPaged={waitPaged}
+      />
     </div>
   );
 };
