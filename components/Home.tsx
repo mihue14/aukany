e8f687c98d48ae83b2836f83c66390079f85aa5a
mihue14 <samirjose675@gmail.com>
2022-12-30 21:39:49 -0300

    Responsive arreglado

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import background from "../assets/background.jpg";
-import { Babang } from "../fonts";
+import { Flathead } from "../fonts";
 
 const Home = () => {
   return (
@@ -11,7 +11,7 @@ const Home = () => {
         className="h-screen w-screen object-cover"
       />
       <div
-        className={`absolute text-6xl top-1/3 left-1/3 -translate-x-1/4 -translate-y-1/2 text-white font-semibold ${Babang.className} text-xl lg:left-1/2 lg:-translate-x-1/2 lg:text-5xl md:text-5xl md:left-0 md:translate-x-[11%]`}
+        className={`absolute text-5xl top-1/3 left-1/3 -translate-x-1/4 -translate-y-1/2 text-white font-semibold ${Flathead.className} text-xl lg:left-1/2 lg:-translate-x-1/2 lg:text-5xl md:text-5xl md:left-0 md:translate-x-[11%]`}
       >
         ¡Sin turnos todo el mes de diciembre!
       </div>
