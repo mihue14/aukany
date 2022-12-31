c155dbb29c4963af53bbb989d01e7de255a7f3f7
mihue14 <samirjose675@gmail.com>
2022-12-31 18:52:43 -0300

    Filtro para buscar reservas por fecha

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { Flathead } from "../fonts";
-import { filterByDate } from "../hooks/filterByDate";
+import { useAppDispatch } from "../hooks";
+import { getBookingsByDate } from "../redux/actions/bookingsActions";
 
 const Filters = () => {
+  const dispatch = useAppDispatch();
+
   return (
     <div>
-      <div className="grid">
-        <span className={`${Flathead.className} mt-5 mx-auto`}>
-          Buscar por fecha
-        </span>
+      <div className="grid justify-center">
+        <span className={`${Flathead.className} mt-5`}>Buscar por fecha</span>
         <input
           type="date"
-          name=""
-          id=""
-          onChange={(e) => filterByDate(e.target.value)}
-          className="mb-5 mx-auto"
+          onChange={(e) => {
+            dispatch(getBookingsByDate(e.target.value));
+          }}
+          className="mb-5 "
         />
       </div>
     </div>
