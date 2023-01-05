3543c7543471181c14739281fa3482d00d6cc816
mihue14 <samirjose675@gmail.com>
2023-01-04 22:23:34 -0300

    Nuevos types

--- redux/types.ts
+++ redux/types.ts
@@ -1,6 +1,9 @@
 export const GET_ALL_BOOKINGS = "GET_ALL_BOOKINGS";
 export const GET_BOOKINGS = "GET_BOOKINGS";
 export const GET_BOOKINGS_BY_DATE = "GET_BOOKINGS_BY_DATE";
+export const GET_BOOKINGS_BY_NAME = "GET_BOOKINGS_BY_NAME";
+export const GET_BOOKINGS_BY_LASTNAME = "GET_BOOKINGS_BY_LASTNAME";
+export const GET_BOOKINGS_BY_SERVICE = "GET_BOOKINGS_BY_SERVICE";
 
 interface GetAllBookings {
   type: typeof GET_ALL_BOOKINGS;
@@ -14,10 +17,48 @@ interface GetBookings {
 
 interface GetBookingsByDate {
   type: typeof GET_BOOKINGS_BY_DATE;
-  payload: string;
+  payload: {
+    name: string;
+    lastname: string;
+    date: string;
+    service: string;
+  };
+}
+
+interface GetBookingsByName {
+  type: typeof GET_BOOKINGS_BY_NAME;
+  payload: {
+    name: string;
+    lastname: string;
+    date: string;
+    service: string;
+  };
+}
+
+interface GetBookingsByLastname {
+  type: typeof GET_BOOKINGS_BY_LASTNAME;
+  payload: {
+    name: string;
+    lastname: string;
+    date: string;
+    service: string;
+  };
+}
+
+interface GetBookingsByService {
+  type: typeof GET_BOOKINGS_BY_SERVICE;
+  payload: {
+    name: string;
+    lastname: string;
+    date: string;
+    service: string;
+  };
 }
 
 export type BookingsDispatchTypes =
   | GetAllBookings
   | GetBookings
-  | GetBookingsByDate;
+  | GetBookingsByDate
+  | GetBookingsByName
+  | GetBookingsByLastname
+  | GetBookingsByService;
