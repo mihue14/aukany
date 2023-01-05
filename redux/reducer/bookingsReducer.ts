8675f44df3defd3ae5f126fbc8efff352b1c0b37
mihue14 <samirjose675@gmail.com>
2023-01-04 22:23:11 -0300

    Reducer con filtrado combinado

--- redux/reducer/bookingsReducer.ts
+++ redux/reducer/bookingsReducer.ts
@@ -1,9 +1,15 @@
+import { IBookings } from "../../interfaces/bookings";
 import {
   BookingsDispatchTypes,
   GET_ALL_BOOKINGS,
   GET_BOOKINGS,
   GET_BOOKINGS_BY_DATE,
+  GET_BOOKINGS_BY_LASTNAME,
+  GET_BOOKINGS_BY_NAME,
+  GET_BOOKINGS_BY_SERVICE,
 } from "./../types";
+import { removeAccents } from "../../utils/accents";
+
 const initialState = {
   allBookings: [],
   bookings: [],
@@ -13,6 +19,7 @@ const bookingsReducer = (
   state = initialState,
   action: BookingsDispatchTypes
 ) => {
+  let AllBookings = state.allBookings;
   switch (action.type) {
     case GET_ALL_BOOKINGS:
       return {
@@ -28,13 +35,108 @@ const bookingsReducer = (
       };
 
     case GET_BOOKINGS_BY_DATE:
-      let AllBookings = state.allBookings;
-      if (action.payload === "undefined/undefined/")
-        return { ...state, bookings: AllBookings };
-      let filter = AllBookings.filter(
-        (elem: { day: string }) => elem.day === action.payload
-      );
-      return { ...state, bookings: filter };
+      action.payload.name = removeAccents(action.payload.name);
+      action.payload.lastname = removeAccents(action.payload.lastname);
+      let filterDate = AllBookings.filter((elem: IBookings) =>
+        action.payload.date === "undefined/undefined/" ||
+        action.payload.date === ""
+          ? AllBookings
+          : elem.day === action.payload.date
+      )
+        .filter((elem: IBookings) =>
+          removeAccents(elem.name)
+            .toLowerCase()
+            .includes(action.payload.name.toLowerCase())
+        )
+        .filter((elem: IBookings) =>
+          removeAccents(elem.lastname)
+            .toLowerCase()
+            .includes(action.payload.lastname.toLowerCase())
+        )
+        .filter((elem: IBookings) =>
+          action.payload.service === "DEFAULT"
+            ? AllBookings
+            : elem.service === action.payload.service
+        );
+      return { ...state, bookings: filterDate };
+
+    case GET_BOOKINGS_BY_NAME:
+      action.payload.name = removeAccents(action.payload.name);
+      action.payload.lastname = removeAccents(action.payload.lastname);
+      let filterName = AllBookings.filter((elem: IBookings) =>
+        removeAccents(elem.name)
+          .toLowerCase()
+          .includes(action.payload.name.toLowerCase())
+      )
+        .filter((elem: IBookings) =>
+          removeAccents(elem.lastname)
+            .toLowerCase()
+            .includes(action.payload.lastname.toLowerCase())
+        )
+        .filter((elem: IBookings) =>
+          action.payload.date === "undefined/undefined/" ||
+          action.payload.date === ""
+            ? AllBookings
+            : elem.day === action.payload.date
+        )
+        .filter((elem: IBookings) =>
+          action.payload.service === "DEFAULT"
+            ? AllBookings
+            : elem.service === action.payload.service
+        );
+      return { ...state, bookings: filterName };
+
+    case GET_BOOKINGS_BY_LASTNAME:
+      action.payload.name = removeAccents(action.payload.name);
+      action.payload.lastname = removeAccents(action.payload.lastname);
+      let filterLastname = AllBookings.filter((elem: IBookings) =>
+        removeAccents(elem.lastname)
+          .toLowerCase()
+          .includes(action.payload.lastname.toLowerCase())
+      )
+        .filter((elem: IBookings) =>
+          removeAccents(elem.name)
+            .toLowerCase()
+            .includes(action.payload.name.toLowerCase())
+        )
+        .filter((elem: IBookings) =>
+          action.payload.date === "undefined/undefined/" ||
+          action.payload.date === ""
+            ? AllBookings
+            : elem.day === action.payload.date
+        )
+        .filter((elem: IBookings) =>
+          action.payload.service === "DEFAULT"
+            ? AllBookings
+            : elem.service === action.payload.service
+        );
+      return { ...state, bookings: filterLastname };
+
+    case GET_BOOKINGS_BY_SERVICE:
+      action.payload.name = removeAccents(action.payload.name);
+      action.payload.lastname = removeAccents(action.payload.lastname);
+      let filterService = AllBookings.filter((elem: IBookings) =>
+        removeAccents(elem.lastname)
+          .toLowerCase()
+          .includes(action.payload.lastname.toLowerCase())
+      )
+        .filter((elem: IBookings) =>
+          removeAccents(elem.name)
+            .toLowerCase()
+            .includes(action.payload.name.toLowerCase())
+        )
+        .filter((elem: IBookings) =>
+          action.payload.date === "undefined/undefined/" ||
+          action.payload.date === ""
+            ? AllBookings
+            : elem.day === action.payload.date
+        )
+        .filter((elem: IBookings) =>
+          action.payload.service === "DEFAULT"
+            ? AllBookings
+            : elem.service === action.payload.service
+        );
+      return { ...state, bookings: filterService };
 
     default:
       return state;
