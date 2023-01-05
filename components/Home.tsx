ce33c8d3b33bb55f007de0e9b84317e32465edbe
mihue14 <samirjose675@gmail.com>
2023-01-04 23:12:38 -0300

    Responsive arreglado

--- components/Home.tsx
+++ components/Home.tsx
@@ -11,7 +11,7 @@ const Home = () => {
         className="h-screen w-screen object-cover"
       />
       <div
-        className={`absolute text-5xl top-1/3 left-1/3 -translate-x-1/4 -translate-y-1/2 text-white font-semibold ${Flathead.className} text-xl lg:left-1/2 lg:-translate-x-1/2 lg:text-5xl md:text-5xl md:left-0 md:translate-x-[11%]`}
+        className={`absolute text-5xl top-1/3 left-1/3 -translate-x-1/4 -translate-y-1/2 text-white font-semibold ${Flathead.className} lg:left-1/2 lg:-translate-x-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2`}
       >
         ¡Nuevos precios!
       </div>
